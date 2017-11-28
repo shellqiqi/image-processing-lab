c6d800347ec76c6864dafdda428cfb392562ea77
shell7 <shellqiqi@outlook.com>
2017-11-28 13:11:32 +0800

    Delete duplicate variable

--- ImageProcessing/ImageProcessing.cs
+++ ImageProcessing/ImageProcessing.cs
@@ -167,6 +167,31 @@ namespace ImageProcessing
             return bitmap;
         }
 
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
         public static Image CLAHE(Image src, int block = 8)
         {
             Bitmap srcBitmap = new Bitmap(src);
@@ -197,7 +222,7 @@ namespace ImageProcessing
                     {
                         for (int x = start_x; x < end_x; x++)
                         {
-                            int grayValue = srcBitmap.GetPixel(x, y).R;
+                            int grayValue = (int)(srcBitmap.GetPixel(x, y).GetBrightness() * 255);
                             PDF[xBlockIndex, yBlockIndex, grayValue]++;
                         }
                     }
@@ -241,30 +266,33 @@ namespace ImageProcessing
             {
                 for (int x = 0; x < claheWidth; x++)
                 {
+                    int srcGrayValue = (int)(srcBitmap.GetPixel(x, y).GetBrightness() * 255);
+                    double srcHue = srcBitmap.GetPixel(x, y).GetHue();
+                    double srcSaturation = srcBitmap.GetPixel(x, y).GetSaturation();
                     /* corners */
                     // top left corner
                     if (x <= blockWidth / 2 && y <= blockHeight / 2)
                     {
-                        int gray = (int)(CDF[0, 0, srcBitmap.GetPixel(x, y).R] * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness = CDF[0, 0, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // top right corner
                     else if (x >= claheWidth - blockWidth / 2 && y <= blockHeight / 2)
                     {
-                        int gray = (int)(CDF[block - 1, 0, srcBitmap.GetPixel(x, y).R] * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness = CDF[block - 1, 0, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // bottom left corner
                     else if (x <= blockWidth / 2 && y >= claheHeight - blockHeight / 2)
                     {
-                        int gray = (int)(CDF[0, block - 1, srcBitmap.GetPixel(x, y).R] * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness = CDF[0, block - 1, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // bottom right corner
                     else if (x >= claheWidth - blockWidth / 2 && y >= claheHeight - blockHeight / 2)
                     {
-                        int gray = (int)(CDF[block - 1, block - 1, srcBitmap.GetPixel(x, y).R] * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness = CDF[block - 1, block - 1, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     /* edges */
                     // left edge
@@ -276,8 +304,10 @@ namespace ImageProcessing
                         double q = (y - ((double)yBlockIndex * blockHeight + blockHeight / 2)) / blockHeight;
                         double p = 1 - q;
 
-                        int gray = (int)((p * CDF[xBlockIndex, yBlockIndex, srcBitmap.GetPixel(x, y).R] + q * CDF[xBlockIndex, yBlockIndex + 1, srcBitmap.GetPixel(x, y).R]) * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness =
+                            p * CDF[xBlockIndex, yBlockIndex, srcGrayValue] +
+                            q * CDF[xBlockIndex, yBlockIndex + 1, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // right edge
                     else if (x >= (claheWidth - blockWidth / 2))
@@ -288,8 +318,10 @@ namespace ImageProcessing
                         double q = (y - ((double)yBlockIndex * blockHeight + blockHeight / 2)) / blockHeight;
                         double p = 1 - q;
 
-                        int gray = (int)((p * CDF[xBlockIndex, yBlockIndex, srcBitmap.GetPixel(x, y).R] + q * CDF[xBlockIndex, yBlockIndex + 1, srcBitmap.GetPixel(x, y).R]) * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness =
+                            p * CDF[xBlockIndex, yBlockIndex, srcGrayValue] +
+                            q * CDF[xBlockIndex, yBlockIndex + 1, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // top edge
                     else if (y <= blockHeight / 2)
@@ -300,8 +332,10 @@ namespace ImageProcessing
                         double q = (x - ((double)xBlockIndex * blockWidth + blockWidth / 2)) / blockWidth;
                         double p = 1 - q;
 
-                        int gray = (int)((p * CDF[xBlockIndex, yBlockIndex, srcBitmap.GetPixel(x, y).R] + q * CDF[xBlockIndex + 1, yBlockIndex, srcBitmap.GetPixel(x, y).R]) * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness =
+                            p * CDF[xBlockIndex, yBlockIndex, srcGrayValue] +
+                            q * CDF[xBlockIndex + 1, yBlockIndex, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     // bottom edge
                     else if (y >= (claheHeight - blockHeight / 2))
@@ -312,8 +346,10 @@ namespace ImageProcessing
                         double q = (x - ((double)xBlockIndex * blockWidth + blockWidth / 2)) / blockWidth;
                         double p = 1 - q;
 
-                        int gray = (int)((p * CDF[xBlockIndex, yBlockIndex, srcBitmap.GetPixel(x, y).R] + q * CDF[xBlockIndex + 1, yBlockIndex, srcBitmap.GetPixel(x, y).R]) * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness =
+                            p * CDF[xBlockIndex, yBlockIndex, srcGrayValue] +
+                            q * CDF[xBlockIndex + 1, yBlockIndex, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                     /* Inner */
                     else
@@ -325,11 +361,12 @@ namespace ImageProcessing
                         double px = 1 - qx;
                         double qy = (y - ((double)yBlockIndex * blockHeight + blockHeight / 2)) / blockHeight;
                         double py = 1 - qy;
-                        int gray = (int)((qx * qy * CDF[xBlockIndex + 1, yBlockIndex + 1, srcBitmap.GetPixel(x, y).R] +
-                                            px * py * CDF[xBlockIndex, yBlockIndex, srcBitmap.GetPixel(x, y).R] +
-                                            px * qy * CDF[xBlockIndex, yBlockIndex + 1, srcBitmap.GetPixel(x, y).R] +
-                                            qx * py * CDF[xBlockIndex + 1, yBlockIndex, srcBitmap.GetPixel(x, y).R]) * 255);
-                        srcBitmap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        double brightness =
+                            qx * qy * CDF[xBlockIndex + 1, yBlockIndex + 1, srcGrayValue] +
+                            px * py * CDF[xBlockIndex, yBlockIndex, srcGrayValue] +
+                            px * qy * CDF[xBlockIndex, yBlockIndex + 1, srcGrayValue] +
+                            qx * py * CDF[xBlockIndex + 1, yBlockIndex, srcGrayValue];
+                        srcBitmap.SetPixel(x, y, ColorFromHSV(srcHue, srcSaturation, brightness));
                     }
                 }
             }
